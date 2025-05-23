4e07d304412eb30bb699b84e7e16baf79192c1a3
dz1kill <dz1kill06@gmail.com>
2025-05-23 15:27:45 +0300

    save

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import { ROUTES } from "../../utils/routes";
+import styles from "../../styles/Home.module.css";
 
 const Home = () => {
   const navigate = useNavigate();
