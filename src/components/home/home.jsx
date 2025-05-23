84096366dbfeef363ce5ee574a43eb3fec523096
dz1kill <dz1kill06@gmail.com>
2025-05-23 16:02:41 +0300

    refactor

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import { ROUTES } from "../../utils/routes";
-import styles from "../../styles/Home.module.css";
 
 const Home = () => {
   const navigate = useNavigate();
